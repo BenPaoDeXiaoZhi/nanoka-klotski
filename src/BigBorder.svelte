c6e4b535e4115155ab2819085c7d558a4d691e7f
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-22 07:23:37 +0800

    更新 BigBorder.svelte

--- src/BigBorder.svelte
+++ src/BigBorder.svelte
@@ -8,27 +8,16 @@
   const { children, size }: Props = $props();
 </script>
 
-<div class="out">
-  <div class="in">
-    {@render children()}
-  </div>
+<div>
+  {@render children()}
 </div>
 
 <style>
   div.out {
-    width: 31em;
-    height: 31em;
+    width: 30em;
+    height: 30em;
     border-radius: 0.5em;
     background-color: #e0e2e4;
-  }
-
-  div.in {
-    width: 30em;
-    aspect-ratio: 1;
-    position: relative;
-    top: 0.5em;
-    left: 0.5em;
-    z-index: 1;
-    background-color: #e0e2e4;
+    padding: 0.5em
   }
 </style>
